72de45cb39b5ad7da3af2d7f6c67c75dbfb7b235
Angkit Khadka <khadkaankit85@gmail.com>
2025-02-01 01:12:16 -0500

    somehow back to two sum

--- src/EasyMedium/TwoSum.ts
+++ src/EasyMedium/TwoSum.ts
@@ -3,8 +3,38 @@
  *You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *You can return the answer in any order.
  */
-function twoSum(nums: number[], target: number): number[] {
-  return [7, 8];
+
+// brute force
+function twoSumBrute(nums: number[], target: number): number[] {
+  //map over the element, for each element, try to add it to another element and then  check the sum, if the sum is the target, then return the indices
+  let firstIn = -1;
+  let secondIn = -1;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        firstIn = i;
+        secondIn = j;
+      }
+    }
+  }
+  return [firstIn, secondIn];
 }
 
-twoSum();
+const nums = [2, 7, 11, 15];
+twoSumBrute(nums, 9);
+
+function twoSum(nums: number[], target: number): number[] {
+  let hashmap = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    let numNeeded = target - nums[i];
+    if (hashmap.has(numNeeded)) {
+      return [hashmap.get(numNeeded)!, i];
+    }
+    hashmap.set(nums[i], i);
+  }
+
+  return [];
+}
+
+console.log(twoSum(nums, 9));
