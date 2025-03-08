f8deaa20692d40fe3b70da41db3a725bec8a66af
Angkit Khadka <khadkaankit85@gmail.com>
2025-03-08 03:29:46 -0500

    longest Repeating character with replacement, leetcode medium

--- src/Blind75/longestRepeatingCharReplacement.ts
+++ src/Blind75/longestRepeatingCharReplacement.ts
@@ -10,9 +10,25 @@ Explanation: Either replace the 'X's with 'Y's, or replace the 'Y's with 'X's.
 
 class Solution {
   characterReplacement(s: string, k: number) {
-    const arr = Array.from({ length: 26 }, (_, i) =>
-      String.fromCharCode(i + 65),
-    );
-    const map = new Map(arr.map((char, index) => [char, index]));
+    let store: Map<string, number> = new Map();
+    let maxFrequency = 0;
+    let leftPointer = 0;
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+      store.set(s[i], (store.get(s[i]) || 0) + 1);
+      maxFrequency = Math.max(maxFrequency, store.get(s[i]) || 0);
+      //if the letter is not valid to put as a substring
+      if (i - leftPointer + 1 - maxFrequency > k) {
+        store.set(s[leftPointer], store.get(s[leftPointer])! - 1);
+        leftPointer++;
+      }
+      result = Math.max(i - leftPointer + 1, result);
+    }
+    return result;
   }
 }
+const solution = new Solution();
+let s = "XYYX";
+let k = 2;
+
+console.log(solution.characterReplacement(s, k));
