ad46f0389d3f6579f0245bb3906c6d77c98aaf49
Angkit Khadka <khadkaankit85@gmail.com>
2025-03-24 23:56:57 -0400

    Reverse a linked list

---
+++ src/Blind75/medianOfTwoSortedArrays.ts
@@ -0,0 +1,19 @@
+/*
+Median of Two Sorted Arrays
+You are given two integer arrays nums1 and nums2 of size m and n respectively, where each is sorted in ascending order. Return the median value among all elements of the two arrays.
+Your solution must run in O(log(m+n)) time.
+Example 1:
+Input: nums1 = [1,2], nums2 = [3]
+Output: 2.0
+Explanation: Among [1, 2, 3] the median is 2.
+*/
+
+class Solution {
+  /**
+   * @param {number[]} nums1
+   * @param {number[]} nums2
+   * @return {number}
+   */
+  findMedianSortedArrays(nums1: number[], nums2: number[]) {
+  }
+}
---
+++ src/Blind75/reverseLinkedList.ts
@@ -0,0 +1,30 @@
+/*
+Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+Example 1:
+Input: head = [0,1,2,3]
+Output: [3,2,1,0]
+*/
+
+class ListNode {
+  val
+  next: ListNode | null
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  reverseList(head: ListNode) {
+    let prev = null
+    let current = head
+    if (!current) return current
+    while (current) {
+      const next = current.next
+      current.next = prev
+      prev = current
+      current = next
+    }
+    return prev
+  }
+}
