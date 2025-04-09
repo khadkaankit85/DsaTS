f50e361bf39d8c03b5bca0a404c9e232e0bb802a
khadkaankit85 <khadkaankit85@gmail.com>
2025-04-09 15:37:57 -0400

    easy peasy, reverse a linked list with two pointer, prev and current

---
+++ src/trash/reverseLinkedList.ts
@@ -0,0 +1,13 @@
+class Solution {
+  reverseList(head) {
+    let prev = null;
+    let current = head;
+    while (current) {
+      const next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    return prev;
+  }
+}
