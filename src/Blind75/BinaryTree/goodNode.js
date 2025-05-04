fd0a889af7e90ecc8ee53358aa77cae33442b31c
khadkaankit85 <khadkaankit85@gmail.com>
2025-05-04 11:57:25 -0400

    (binary_tree) deeper into find good node, recursion call stack

--- src/Blind75/BinaryTree/goodNode.js
+++ src/Blind75/BinaryTree/goodNode.js
@@ -9,32 +9,65 @@ Output: 3
 
 /**
  * Definition for a binary tree node.
- * class TreeNode {
- *     constructor(val = 0, left = null, right = null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
  */
-
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
 class Solution {
   /**
    * @param {TreeNode} root
    * @return {number}
    */
   goodNodes(root) {
+    //lets try to traversals, first one is parent, left node and right node traversal, known as pre-order traversal
+    //pass the maxvalue so far
     function dfs(node, maxSofar) {
-      // base case: if the root is null then the count of good node will be retured 0
       if (!node) return 0;
-
-      //for this path, the value of good is 0 or 1 depending upont hte value of the root
-      let good = node.val >= maxSofar ? 1 : 0;
+      //for the current level, current path or whatever, declare this variable
+      //the scope of this result variable is
+      let result = node.val >= maxSofar ? 1 : 0;
       maxSofar = Math.max(maxSofar, node.val);
-      good += dfs(node.left, maxSofar);
-      good += dfs(node.right, maxSofar);
-      return good;
+      //do the same thing on the left child side
+      result += dfs(node.left, maxSofar);
+      //do the same thing on the right child side
+      result += dfs(node.right, maxSofar);
+      return result;
     }
     return dfs(root, root.val);
   }
+}
+
+// -------- RUN TEST BELOW --------
+const input = [2, 1, 1, 3, null, 1, 5];
+const root = buildTree(input);
+const sol = new Solution();
+const result = sol.goodNodes(root);
+console.log("Good Nodes:", result);
+
+// Convert array (level order) to binary tree
+function buildTree(arr) {
+  if (!arr.length) return null;
+  const root = new TreeNode(arr[0]);
+  const queue = [root];
+  let i = 1;
+
+  while (queue.length && i < arr.length) {
+    const current = queue.shift();
+    if (arr[i] !== null) {
+      current.left = new TreeNode(arr[i]);
+      queue.push(current.left);
+    }
+    i++;
+    if (i < arr.length && arr[i] !== null) {
+      current.right = new TreeNode(arr[i]);
+      queue.push(current.right);
+    }
+    i++;
+  }
+
+  return root;
 }
